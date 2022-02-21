218b0839c41172985631048f6cbc7fe0ba2d25cc
Uzair Hussain <uzairhussain98@hotmail.com>
2022-02-21 23:48:56 +0500

    completed app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect,useState} from "react"
+import { Contract,providers} from "ethers"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [isWalletInstalled,setIsWalletInstalled]= useState(false);
+    const [account, setAccount]= useState(null);
+
+    useEffect(()=> {
+      if (window.ethereum){
+          setIsWalletInstalled(true);
+      }
+    },[]);
+
+
+    async function connectWallet(){
+      window.ethereum
+      .request({
+        method:"eth_requestAccounts",
+      })
+      .then((accounts)=>{
+        setAccount(accounts[0])
+      })
+      .catch((error)=>{
+        alert("Something Went Wrong")
+      });
+    }
+
+
+    if(account===null){
+      return (
+        <div className="App">
+          {
+            isWalletInstalled ? (
+            <button className="btn" onClick={connectWallet}>Connect Metamask</button>)
+          : (
+          <p>Install Metamask Wallet</p>
+          )
+           }
+
+        </div>
+
+      );
+
+
+    }
+
+    return(
+      <div className="App"> 
+      <p>Connected As {account} </p>
+      </div>
+    );
+
+
+
 }
 
 export default App;
